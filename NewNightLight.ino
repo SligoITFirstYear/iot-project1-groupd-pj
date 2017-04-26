53eb1fe22bc726339cc78d6c8e8d9c8abf5b0e79
JasonS97 <s00169725@mail.itsligo.ie>
2017-04-26 16:33:56 +0100

    Update NewNightLight.ino

--- NewNightLight.ino
+++ NewNightLight.ino
@@ -1,26 +1,3 @@
-/*
-  Arduino Yún Bridge example
-
-  This example for the Arduino Yún shows how to use the
-  Bridge library to access the digital and analog pins
-  on the board through REST calls. It demonstrates how
-  you can create your own API when using REST style
-  calls through the browser.
-
-  Possible commands created in this shetch:
-
-  "/arduino/digital/13"     -> digitalRead(13)
-  "/arduino/digital/13/1"   -> digitalWrite(13, HIGH)
-  "/arduino/analog/2/123"   -> analogWrite(2, 123)
-  "/arduino/analog/2"       -> analogRead(2)
-  "/arduino/mode/13/input"  -> pinMode(13, INPUT)
-  "/arduino/mode/13/output" -> pinMode(13, OUTPUT)
-
-  This example code is part of the public domain
-
-  http://www.arduino.cc/en/Tutorial/Bridge
-
-*/
 
 #include <Bridge.h>
 #include <BridgeServer.h>
@@ -32,11 +9,8 @@ BridgeServer server;
 
 void setup() {
   // Bridge startup
-  pinMode(13, OUTPUT);
   pinMode(3, OUTPUT);
-  digitalWrite(13, LOW);
   Bridge.begin();
-  digitalWrite(13, HIGH);
 
   // Listen for incoming connection only from localhost
   // (no one from the external network could connect)
